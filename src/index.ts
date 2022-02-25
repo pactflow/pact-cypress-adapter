e9f0624b0bd01646c861a1dd4b0d816b887d1de9
B3nnyL <shuyinglin47@gmail.com>
2022-02-25 14:50:54 +1100

    feat: add usePactGet and usePactRequest

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { aliasType, pactflowConfig } from 'types'
+import { aliasType, AnyObject, pactflowConfig, XHRRequestAndResponse } from 'types'
 import { formatAlias, writePact } from './utils'
 
 declare global {
@@ -6,6 +6,8 @@ declare global {
   namespace Cypress {
     interface Chainable {
       usePactWait: (alias: aliasType, pactflowConfig: pactflowConfig) => Chainable
+      usePactRequest: (option: AnyObject, alias: string) => Chainable
+      usePactGet: (alias: string, pactflowConfig: pactflowConfig) => Chainable
     }
   }
 }
@@ -36,4 +38,37 @@ const usePactWait = (alias: aliasType, pactflowConfig: pactflowConfig) => {
   }
 }
 
+const requestDataMap: AnyObject = {}
+
+const usePactGet = (alias: string, pactflowConfig: pactflowConfig) => {
+  const formattedAlias = formatAlias(alias)
+  const testCaseTitle = Cypress.currentTest.title
+  const filePath = constructFilePath(pactflowConfig)
+  formattedAlias.forEach((alias) => {
+    cy.get(alias).then((response: any) => {
+      const fullRequestAndResponse = {
+        request: {
+          method: requestDataMap[alias].method,
+          url: requestDataMap[alias].url,
+          headers: response.requestHeaders
+        },
+        response: {
+          body: response.body,
+          status: response.status,
+          headers: response.headers,
+          statusText: response.statusText
+        }
+      } as XHRRequestAndResponse
+      writePact(filePath, fullRequestAndResponse, `${testCaseTitle}-${alias}`, pactflowConfig)
+    })
+  })
+}
+
+const usePactRequest = (option: AnyObject, alias: string) => {
+  cy.request(option).as(alias)
+  requestDataMap[`@${alias}`] = option
+}
+
 Cypress.Commands.add('usePactWait', usePactWait)
+Cypress.Commands.add('usePactRequest', usePactRequest)
+Cypress.Commands.add('usePactGet', usePactGet)
