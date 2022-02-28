1e9f4e699c2b3b0e7bf4107c28b0442b503601af
B3nnyL <shuyinglin47@gmail.com>
2022-02-28 14:31:48 +1100

    choreL refactor plugin and add tests

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,15 +3,9 @@
  */
 
 // eslint-disable-next-line no-unused-vars
+const { readFileAsync } = require('./utils')
 module.exports = (on: any, config: any, fs: any) => {
-  const fileExists = async (filename: string) => !!(await fs.promises.stat(filename).catch((e: any) => false))
-  const readFile = async (filename: string) => {
-    if (await fileExists(filename)) {
-      const data = await fs.promises.readFile(filename, 'utf8')
-      return data
-    }
-    return null
-  }
+  const readFile = (filename: string) => readFileAsync(fs.promises, filename)
   const removePactDir = () => {
     fs.promises.rm('cypress/pacts', { recursive: true, force: true }).then(() => {
       console.log('Clear up pacts')
