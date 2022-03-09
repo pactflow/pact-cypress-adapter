7f0173cd0ae8302d3485089dd44f9073bfb8f319
B3nnyL <13126965+B3nnyL@users.noreply.github.com>
2022-03-09 15:07:51 +1100

    chore: update example and readme

--- example/todo-example/cypress/support/index.js
+++ example/todo-example/cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@pactflow/pact-cypress-adaptor'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
