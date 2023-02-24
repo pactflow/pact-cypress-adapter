13dc54754d562b96f6b9cc7c52737c3b15e5e933
B3nnyL <13126965+B3nnyL@users.noreply.github.com>
2023-02-24 14:40:14 +1100

    chore: add example for cypress 10

---
+++ example-cypress-10/todo-example/cypress/e2e/todoGet.cy.js
@@ -0,0 +1,28 @@
+
+describe('example to-do app', () => {
+  before(() => {
+    cy.setupPact('ui-consumer', 'todo-api')
+    cy.usePactRequest(
+      {
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/todos',
+        headers: {
+          'x-pactflow': 'blah',
+          'ignore-me': 'ignore',
+          'ignore-me-globally': 'ignore'
+        }
+      },
+      'getTodosGet'
+    )
+    cy.setupPactHeaderBlocklist(['ignore-me'])
+  })
+
+  it('shows todo', () => {
+  })
+
+  after(() => {
+    cy.usePactGet('getTodosGet')
+      .its('response.statusCode')
+      .should('eq', 200)
+  })
+})
